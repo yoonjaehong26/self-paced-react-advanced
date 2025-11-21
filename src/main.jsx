26010ebcb665da11757a9f716649c5a299c8a388
“윤재홍” <a287933@naver.com>
2025-11-21 16:46:13 +0900

    feat():isRestaurantDetailModalOpen 상태를 Context API로 제공하게 변경

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import theme from './theme';
+import { ModalProvider } from './contexts/ModalContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <ModalProvider>
+        <App />
+      </ModalProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
